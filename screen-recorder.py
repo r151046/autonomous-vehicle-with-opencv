f55d14a70860840ae089d185b133992c179d32b3
r151046 <46886702+r151046@users.noreply.github.com>
2020-04-19 13:26:51 +0530

    Add files via upload

---
+++ screen-recorder.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+import pyscreenshot as pys
+#fourcc = cv2.cv.CV_FOURCC(*'XVID') 
+#video_writer = cv2.VideoWriter("drop.avi", fourcc, 20, (640,480))
+forcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', forcc, 8, (1920,1080))
+
+
+while True:
+    img= pys.grab()
+    img_np=np.array(img)
+
+    #frame= cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+
+    #cv2.imshow('Screen', img_np)
+    out.write(img_np)
+
+
+    if cv2.waitKey(20) & 0xFF==ord('q'):
+        break
+
+out.release()
+cv2.destroyAllWindows()
